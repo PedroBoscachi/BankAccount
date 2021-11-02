8ca29cf28380c5499fb26b5b38da43db70d4624d
Pedro <pedroboscachi@gmail.com>
2021-11-02 13:28:41 -0300

    first commit

---
+++ BankingSystem/Account.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+namespace BankingSystem {
+    class Account {
+
+        public string Name { get; private set; }
+        public int AccountNumber { get; private set; }
+        public double Balance { get; private set; }
+
+        public Account(string name, int number, double balance) {
+            Name = name;
+            AccountNumber = number;
+            Balance = balance;
+        }
+
+        public void Deposit(double amount) {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount) {
+            Balance -= amount;
+        }
+
+        public double GetBalance() {
+            return Balance;
+        }
+
+        public string GetName() {
+            return Name;
+        }
+
+        public override string ToString() {
+            return AccountNumber
+                + ", "
+                + Name
+                + ", "
+                + Balance.ToString("F3", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ BankingSystem/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace BankingSystem {
+    class Program {
+        static void Main(string[] args) {
+
+            List<Account> accounts = new List<Account>();
+
+            for (int i = 0; i < 3; i++) {
+                Console.Clear();
+                Console.Write("Enter the name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter the account number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Enter the balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                accounts.Add(new Account(name, number, balance));
+            }
+
+            Console.WriteLine();
+
+            foreach (Account obj in accounts) {
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine();
+
+            bool end = false;
+
+            do {
+                Console.WriteLine();
+                Console.WriteLine("----MENU----");
+                Console.WriteLine("Choose an option: ");
+                Console.WriteLine("1- Show the accounts");
+                Console.WriteLine("2- Deposit");
+                Console.WriteLine("3- Withdraw");
+                Console.WriteLine("4- Leave");
+                Console.Write("-> ");
+                string option = Console.ReadLine();
+
+                switch (option) {
+
+                    case "1":
+                        foreach (Account obj in accounts) {
+                            Console.WriteLine(obj);
+                        }
+                        break;
+                    case "2":
+                        Console.Write("Account number: ");
+                        int searchNumber = int.Parse(Console.ReadLine());
+                        Account option1 = accounts.Find(x => x.AccountNumber == searchNumber);
+                        if (option != null) {
+                            Console.Write("Enter the value: ");
+                            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                            option1.Deposit(value);
+                        }
+                        break;
+                    case "3":
+                        Console.Write("Account number: ");
+                        int searchNumber2 = int.Parse(Console.ReadLine());
+                        Account option2 = accounts.Find(x => x.AccountNumber == searchNumber2);
+                        if (option != null) {
+                            Console.Write("Enter the value: ");
+                            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                            option2.Withdraw(value);
+                        }
+                        break;
+                    case "4":
+                        end = true;
+                        break;
+
+                }
+            } while (!end);
+        }
+    }
+}
